1a59bb2b7f93241ef72990be2522b52506a35f40
mehmet <drmehmet510@gmail.com>
2020-07-08 23:03:21 +0300

    bu dosyayı gönderelim

--- ly.py
+++ ly.py
@@ -1,1 +1,20 @@
-heyyyyyyyyy dostummmmmmmmmm
+#  Task : Estimating the risk of death from coronavirus
+
+age = True  # can be assigned only True/False
+
+chronic = True # can be assigned only True/False
+
+immune = False # can be assigned only True/False
+
+covid_19 = age and chronic or immune
+
+if covid_19 == True :
+
+    print('there is a risk of death')
+
+else :
+
+    print('there is not a risk of death')
+
+
+print(covid_19)
